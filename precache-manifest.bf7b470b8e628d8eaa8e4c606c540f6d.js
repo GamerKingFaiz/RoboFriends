b70c75b890ef68dabc297ded851f6adcbad92e46
GamerKingFaiz <gamerkingfaiz@gmail.com>
2019-08-18 19:36:30 -0700

    Updates

---
+++ precache-manifest.bf7b470b8e628d8eaa8e4c606c540f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d48d68f29921c5c588da",
+    "url": "/RoboFriends/static/js/runtime~main.19bee101.js"
+  },
+  {
+    "revision": "d2c92f29472e757afb3c",
+    "url": "/RoboFriends/static/js/main.de50f251.chunk.js"
+  },
+  {
+    "revision": "24432575f370ed06c9bd",
+    "url": "/RoboFriends/static/js/2.3d00ac19.chunk.js"
+  },
+  {
+    "revision": "d2c92f29472e757afb3c",
+    "url": "/RoboFriends/static/css/main.d6f04260.chunk.css"
+  },
+  {
+    "revision": "24432575f370ed06c9bd",
+    "url": "/RoboFriends/static/css/2.892ccc6b.chunk.css"
+  },
+  {
+    "revision": "1e5b416d735664aaa9b75f1587b0c6c9",
+    "url": "/RoboFriends/index.html"
+  }
+];
